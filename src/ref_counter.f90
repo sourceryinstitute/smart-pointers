e962081de8164164af93abf386abc65ebfca0c05
Tom Clune <thomas.l.clune@nasa.gov>
2020-05-05 21:29:04 -0400

    more print statements

--- src/ref_counter.f90
+++ src/ref_counter.f90
@@ -42,6 +42,7 @@ contains
   subroutine assign (lhs, rhs)
     class (ref_counter), intent(inout) :: lhs
     class (ref_counter), intent(in) :: rhs
+    print*, 'assign in ref_counter'
     lhs%count => rhs%count; lhs%obj => rhs%obj
     call lhs%grab; end subroutine
   recursive subroutine finalize_ref_counter (this)
