82a3e956845e488a06d3767ed0d61ac1429580a6
Damian Rouson <damian@sourceryinstitute.org>
2020-05-05 23:00:16 -0700

    further reduced the reproducer

--- tests/integration/count-leaks/test-count-leaks.f90
+++ tests/integration/count-leaks/test-count-leaks.f90
@@ -1,23 +1,20 @@
 program main
-   use iso_fortran_env   ,only : error_unit ,output_unit
    use iso_c_binding, only : c_double
-   use vector_implementation, only: vector 
-   use faux_cpp_server , only: num_leaks
+   use vector_implementation, only : vector
+   use faux_cpp_server, only : num_leaks
    implicit none
 
-      type(vector)::x,v,g                      ! Position,velocity,gravity 
-      real(c_double)::t=0.,t_final=1.0,dt=0.5 ! Time, end time, time step
-      real(c_double), parameter:: tau_p=0.1    ! Aerodynamic reponse time 
-      real(c_double), parameter:: zero=0._c_double,one=1._c_double
-      x = vector([zero,zero,zero])           ! Object construction
-      call x%force_finalize()                ! Final clean-up
+   type(vector) x
+   real(c_double), parameter :: zero=0._c_double
+   x = vector([zero,zero,zero])
+   call x%force_finalize()
 
-   if (num_leaks()==0) then
-      write(output_unit,*) 
-      write(output_unit,fmt='(a)') "End Result: TEST PASSED" ;
-   else
-      write(error_unit,*) 
-      write(error_unit,fmt='(a)') "End Result: TEST FAILED" ;
-   end if
+   associate(leaks => num_leaks())
+     if (leaks /= 0) then
+       print *,"number of leaks: ",leaks
+       error stop "Test failed."
+     end if
+   end associate
 
-end program main
+   print *,"Test passed."
+end program
