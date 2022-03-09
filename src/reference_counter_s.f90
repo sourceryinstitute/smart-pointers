ba671c3f196ddf26e855eea39ecbd86fbdbeb75f
Damian Rouson <rouson@lbl.gov>
2022-03-08 18:06:59 -0800

    refac(reference_counter): mod func/subr-> mod proc

--- src/reference_counter_s.f90
+++ src/reference_counter_s.f90
@@ -3,26 +3,22 @@ submodule(reference_counter_m) reference_counter_s
 
 contains
 
-  module function construct(object) result(reference_counter)
-    class(freeable_resource_t), intent(in) :: object
-    type(reference_counter_t) reference_counter
+  module procedure construct
     !type(ref_counter), allocatable :: new_ref_counter
     !allocate (new_ref_counter)
     allocate (reference_counter%count_, source=0)
     allocate (reference_counter%object_, source=object)
     call reference_counter%grab
-  end function
+  end procedure
 
-  module subroutine grab(this)
-    class(reference_counter_t), intent(inout) :: this
+  module procedure grab
     if (associated(this%count_)) then
       this%count_ = this%count_ + 1
     else; stop 'Error in grab: count not associated'
     end if
-  end subroutine
+  end procedure
 
-  module subroutine release(this)
-    class (reference_counter_t), intent(inout) :: this
+  module procedure release
      print*,'release:  associated?',associated(this%count_)
     if (associated(this%count_)) then
        print*,'release:  count:',this%count_
@@ -34,20 +30,17 @@ contains
       end if
     else; stop 'Error in release: count not associated'
     end if
-  end subroutine
+  end procedure
 
-  module subroutine assign_reference_counter(lhs, rhs)
-    class (reference_counter_t), intent(inout) :: lhs
-    class (reference_counter_t), intent(in) :: rhs
+  module procedure assign_reference_counter
     print*, 'assign in ref_counter'
     lhs%count_ => rhs%count_; lhs%object_ => rhs%object_
     call lhs%grab
-  end subroutine
+  end procedure
 
-  recursive module subroutine finalize(this)
-     type(reference_counter_t), intent(inout) :: this
+  module procedure finalize
      print*,'called FINAL'
     if (associated(this%count_)) call this%release
-  end subroutine
+  end procedure
 
 end submodule reference_counter_s
