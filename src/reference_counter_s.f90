db0617d1582f6ecb16ca5532227e3c148fa0750f
Damian Rouson <rouson@lbl.gov>
2022-03-08 18:10:56 -0800

    refac(ref_counter): rm debugging output

--- src/reference_counter_s.f90
+++ src/reference_counter_s.f90
@@ -4,10 +4,8 @@ submodule(reference_counter_m) reference_counter_s
 contains
 
   module procedure construct
-    !type(ref_counter), allocatable :: new_ref_counter
-    !allocate (new_ref_counter)
-    allocate (reference_counter%count_, source=0)
-    allocate (reference_counter%object_, source=object)
+    allocate(reference_counter%count_, source=0)
+    allocate(reference_counter%object_, source=object)
     call reference_counter%grab
   end procedure
 
@@ -19,9 +17,7 @@ contains
   end procedure
 
   module procedure release
-     print*,'release:  associated?',associated(this%count_)
     if (associated(this%count_)) then
-       print*,'release:  count:',this%count_
       this%count_ = this%count_ - 1
       if (this%count_ == 0) then
         call this%object_%free_resource
@@ -33,13 +29,11 @@ contains
   end procedure
 
   module procedure assign_reference_counter
-    print*, 'assign in ref_counter'
     lhs%count_ => rhs%count_; lhs%object_ => rhs%object_
     call lhs%grab
   end procedure
 
   module procedure finalize
-     print*,'called FINAL'
     if (associated(this%count_)) call this%release
   end procedure
 
