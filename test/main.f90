b00b98016fe51fa5c27bdb0f84638ce525e614c4
Brad Richardson <everythingfunctional@protonmail.com>
2022-05-09 14:46:38 -0400

    write tests for playing with usage

--- test/main.f90
+++ test/main.f90
@@ -11,15 +11,19 @@ contains
         use ref_reference_test, only: &
                 ref_reference_ref_reference => &
                     test_ref_reference
+        use usage_test, only: &
+                usage_usage => &
+                    test_usage
         use vegetables, only: test_item_t, test_that, run_tests
 
 
 
         type(test_item_t) :: tests
-        type(test_item_t) :: individual_tests(2)
+        type(test_item_t) :: individual_tests(3)
 
         individual_tests(1) = compiler_ref_reference()
         individual_tests(2) = ref_reference_ref_reference()
+        individual_tests(3) = usage_usage()
         tests = test_that(individual_tests)
 
 
