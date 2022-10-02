a96eb318ab224f9b21b835a2e41875b96feca74d
rouson <rouson@lbl.gov>
2022-10-02 23:55:14 +0000

    Deploying to gh-pages from @ sourceryinstitute/smart-pointers@35b4c9fbd083f07a66c7de11103a78fb0c361c8c 🚀

---
+++ src/assert_m.F90
@@ -0,0 +1,26 @@
+module assert_m
+  !! Enforce logical assertions that can be toggled on/off at compile-time
+  !! To turn off assertions, building with the flag -DUSE_ASSERTIONS=.false.
+  implicit none
+
+  private
+  public :: assert
+
+#ifndef USE_ASSERTIONS
+# define USE_ASSERTIONS .true.
+#endif
+  logical, parameter :: enforce_assertions = USE_ASSERTIONS
+
+  interface 
+
+     pure module subroutine assert(assertion, description)
+       !! Error terminate on .false. assertion with the stop code given by description
+       !! With IBM XL Fortran, the stop code is an integer due to for character stop codes being unsupported.
+       implicit none
+       logical, intent(in) :: assertion
+       character(len=*), intent(in) :: description
+     end subroutine
+
+  end interface
+
+end module assert_m
