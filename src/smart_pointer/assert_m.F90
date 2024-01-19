b757a6b5641e98536de54a20c36541121fcc4c24
Damian Rouson <damian@rouson.net>
2024-01-19 15:23:20 -0800

    doc(assert_m): fix typo in comment

--- src/smart_pointer/assert_m.F90
+++ src/smart_pointer/assert_m.F90
@@ -15,7 +15,7 @@ module assert_m
 
      pure module subroutine assert(assertion, description)
        !! Error terminate on .false. assertion with the stop code given by description
-       !! With IBM XL Fortran, the stop code is an integer due to for character stop codes being unsupported.
+       !! With IBM XL Fortran, the stop code is an integer due to character stop codes being unsupported.
        implicit none
        logical, intent(in) :: assertion
        character(len=*), intent(in) :: description
